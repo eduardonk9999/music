519136629e52d346f6d10cf3dbf6eeabcf4189cf
eduardonk9999 <thiago.novak7@gmail.com>
2025-07-23 09:51:53 -0300

    init project

---
+++ src/main/java/org/example/Nota.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Nota {
+    private String altura; // DÓ
+    private String duracao; // Semínima
+
+
+    // exemplo de uso NotaMusical nota = new NotaMusical("Dó", "Semínima");
+}
